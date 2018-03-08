aee7f902f527e6a1dbb01036f5bd505556ce9df8
Sunny Huang <sizzlorox@gmail.com>
2018-03-08 06:32:44 -0300

    removed unused css

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -6,9 +6,6 @@ const Navigation = require('./Navigation');
 const Display = require('./Display');
 const Footer = require('./Footer');
 
-const sideBarLeft = `${styles.aside} ${styles.asideLeft}`;
-const sideBarRight = `${styles.aside} ${styles.asideRight}`;
-
 class App extends React.Component {
   constructor(props) {
     super(props);
