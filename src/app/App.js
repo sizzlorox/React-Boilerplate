0df066a34cb81e27a5f44b2bd3f37486032946ba
Sunny Huang <sizzlorox@gmail.com>
2018-03-09 12:28:01 -0300

    added wrapper style to main div

--- src/app/App.js
+++ src/app/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className={styles.wrapper}>
         <Navigation />
         <Display />
         <Footer />
